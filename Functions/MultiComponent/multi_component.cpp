b6f7d0ed9a1f2b4410e7e084fd9cb601fedb1b31
NathanASC25 <javanathanjchin@gmail.com>
2026-05-30 04:17:37 -0400

    Latest Update

---
+++ Functions/MultiComponent/multi_component.cpp
@@ -0,0 +1,13 @@
+// Include necessary headers
+#include <iostream> // For input/output operations
+#include <iomanip> // For output formatting
+#include <string> // For string data type
+#include <array> // Container array to hold objects
+#include <exception> // Exception utilities library
+using namespace std;
+int main() {
+    string name;
+    cout << "\nHello! What is your name? ";
+    cin >> name;
+    cout << "Your name: " << name << std :: endl;
+}
